680c0e9f3defb586baa84da14d098431bb8e56bc
EdwinGuty <edwingutrou@unisabana.edu.co>
2023-03-15 19:58:32 -0500

    Modificar clase Dividir con otro metodo

--- src/Division.java
+++ src/Division.java
@@ -1,6 +1,10 @@
 public class Division {
-	public void Dividir(int numero_1, int numero_2)
+	public void dividir(int numero_1, int numero_2)
 	{
 		System.out.println(numero_1/numero_2);
+	}
+	public void dividir_1(int numero_1, int numero_3)
+	{
+		System.out.println(numero_1/numero_3);
 	}
 }
